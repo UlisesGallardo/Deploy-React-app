031c8fdfeaeef096199ef4fe21e8f379df7b544f
UlisesGallardo <0229261@up.edu.mx>
2021-11-28 23:19:25 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -13,15 +13,16 @@ mongoose.connect('mongodb+srv://dbUser:UP.2021@cluster0.ytdh1.mongodb.net/NoTask
         console.log("conectado a la base de datos")
     }
 )
-
-var port = process.env.PORT || 8000;
-app.listen(port,()=>{
-    console.log("corriendo en puerto 3000");
-})
-
 app.use("/todos", TodoRoutes);  //Agregando enrutamiento
 
 app.get('/',(req, res)=>{       //Solo para verificar que tengo acceso a la página
     res.send("hola mundo");
 })
+
+var port = process.env.PORT || 3002;
+app.listen(port,()=>{
+    console.log("corriendo en puerto 3000");
+})
+
+
 
